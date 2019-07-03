1d82844451a7fcc09c67ae0616c0f9b77710224e
Dan Li <akiraryo@hotmail.com>
2019-07-03 11:29:12 +0200

    stenlen的实现原理

---
+++ strlen/strlen/strlen.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+
+size_t _Strlen(char* str)
+{
+	assert(str != NULL);
+	size_t counter = 0;
+	char* temp = str;
+
+	while (*str++ != '\0')
+	{
+		counter++;
+	}
+	return counter;
+}
+
+int main()
+{
+	char* str = NULL;
+	printf("%d", _Strlen(str));
+}
